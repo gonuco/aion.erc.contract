306b5a42c7c59dcf267b753155905feacbe81acd
Yao Sun <mr.sun.yao@gmail.com>
2017-10-05 14:59:36 -0400

    Revert "Merge branch 'master' into master"
    This reverts commit 01cc08daf3501813794e5f5d45837270f39746e6, reversing
    changes made to b671849624602cbde70dd6e6c5ca022ba7fdfee1.

--- audit/test/ControllerEventDefinitions.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.4.10;
-
-contract ControllerEventDefinitions {
-    /**
-     * An internal burn event, emitted by the controller contract
-     * which the bridges could be listening to.
-     */
-    event ControllerBurn(address indexed from, bytes32 indexed to, uint value);
-}
--- audit/test/IToken.sol
+++
@@ -1,8 +0,0 @@
-// Copyright New Alchemy Limited, 2017. All rights reserved.
-
-pragma solidity >=0.4.10;
-
-contract IToken {
-    function transfer(address _to, uint _value) returns (bool);
-    function balanceOf(address owner) returns(uint);
-}
